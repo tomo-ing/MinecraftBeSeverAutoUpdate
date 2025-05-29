6b8739ea697a710f0ba21d5d1e5b0a8ea514b6f8
tomo_ing <tomoinate@gmail.com>
2025-05-29 15:56:18 +0900

    Update has_update.sh

--- has_update.sh
+++ has_update.sh
@@ -12,16 +12,30 @@ if [ ! -f "$CONF_FILE" ]; then
 fi
 
 # 設定ファイルから変数取得
-old_ver=`grep 'old_ver' $CONF_FILE | sed -e 's/[^0-9.]//g'`
-new_ver=`grep 'new_ver' $CONF_FILE | sed -e 's/[^0-9.]//g'`
+# '^キー名=' で行を特定し、'='以降を取得、シングルクォートを除去後、数字とドット以外を削除
+old_ver=$(grep "^old_ver=" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//" | sed 's/[^0-9.]//g')
+new_ver=$(grep "^new_ver=" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//" | sed 's/[^0-9.]//g')
 
-SESSION_NAME=`grep 'SESSION_NAME' $CONF_FILE | sed -e 's/^.\{16\}//' -e 's/.\{1\}$//'`
+# SERVER_DIR の取得 (例: SERVER_DIR='/home/minecraft')
+# '^キー名=' で行を特定し、'='以降を取得、シングルクォートを除去
+SERVER_DIR=$(grep "^SERVER_DIR=" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//")
 
-SERVER_PASS=`grep 'SERVER_PASS' $CONF_FILE | sed -e 's/^.\{16\}//' -e 's/.\{1\}$//'`
+# session_list の取得 (例: SESSION_NAME='s1, s2')
+# '^キー名=' で行を特定し、'='以降を取得、シングルクォートを除去 
+session_list=$(grep "^SESSION_NAME=" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//")
 
-session_list=$(grep "^SESSION_NAME" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//")
+if [ -z "$session_list" ]; then
+  echo "エラー: '$CONF_FILE' に SESSION_NAME の設定が見つからないか、値が空です。" >&2
+  exit 1
+fi
+
+# カンマを区切り文字としてセッション名を一つずつ処理
+#    - `IFS=','` で内部フィールドセパレータをカンマに設定。
+#    - `read -r -a sessions_array <<< "$raw_session_list"` でカンマ区切りの文字列を配列に格納。
+#      (`-r` はバックスラッシュを解釈しない、`-a array` で配列に読み込む)
+IFS=',' read -r -a sessions_array <<< "$session_list"
+
 # 公式サイトからサーバーの最新のバージョン値取得
-
 VERSION=`curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" https://minecraft.net/en-us/download/server/bedrock/ 2>/dev/null | grep bin-linux/bedrock-server | sed -e 's/.*<a href=\"\(https:.*\/bin-linux\/.*\.zip\).*/\1/' -e 's/[^0-9.]//g' -e 's/^.\{2\}//' -e 's/.\{1\}$//'`
 # 現在のサーバーのバージョン値と最新のバージョン値を比較する
 # バージョン値が異なる場合アップデートを行う。そうでない場合はそのままサーバーを起動する
@@ -29,9 +43,9 @@ if [ ${new_ver} != ${VERSION} ]; then
 
   # conf.txtを更新
   OLD_VERSION=${new_ver}
-  sed -e "s/ver='${new_ver}'/ver='${VERSION}'/" -e "s/old_ver='${old_ver}'/old_ver='${OLD_VERSION}'/" ${SERVER_PASS}/conf.txt > tmp
-  mv tmp ${SERVER_PASS}/conf.txt
-  cd ${SERVER_PASS}/
+  sed -e "s/ver='${new_ver}'/ver='${VERSION}'/" -e "s/old_ver='${old_ver}'/old_ver='${OLD_VERSION}'/" ${SERVER_DIR}/conf.txt > tmp
+  mv tmp ${SERVER_DIR}/conf.txt
+  cd ${SERVER_DIR}/
 
   #アップデート用シェルの呼び出し
   ./mcs_update.sh
@@ -41,21 +55,21 @@ else
   # bedrock_serverの起動
   # bedrock_serverのディレクトリ
   # verは設定ファイル内
-  SERVER_DIR01=${SERVER_PASS}/server/bedrock_server${new_ver}
-  # SERVER_DIR02=${SERVER_PASS}/server2/bedrock_server${new_ver}
+  SERVER_DIR01=${SERVER_DIR}/server/bedrock_server${new_ver}
+  # SERVER_DIR02=${SERVER_DIR}/server2/bedrock_server${new_ver}
 
   # SESSION_NAMEは設定ファイル内
   cd ${SERVER_DIR01}
-  cp -pf ${SERVER_PASS}/server.properties server.properties
-  cp -pf ${SERVER_PASS}/permissions.json permissions.json
-  cp -pf ${SERVER_PASS}/allowlist.json allowlist.json
+  cp -pf ${SERVER_DIR}/server.properties server.properties
+  cp -pf ${SERVER_DIR}/permissions.json permissions.json
+  cp -pf ${SERVER_DIR}/allowlist.json allowlist.json
   LD_LIBRARY_PATH=. screen -dmS ${SESSION_NAME} ./bedrock_server > ./bedrock_server01.log &
 
   # sleep 30
 
   # cd ${SERVER_DIR02}
-  # cp -pf ${SERVER_PASS}/server.properties2 server.properties
-  # cp -pf ${SERVER_PASS}/permissions.json permissions.json
+  # cp -pf ${SERVER_DIR}/server.properties2 server.properties
+  # cp -pf ${SERVER_DIR}/permissions.json permissions.json
   # LD_LIBRARY_PATH=. screen -dmS ${SESSION_NAME02} ./bedrock_server > ./bedrock_server02.log &
 
 fi
