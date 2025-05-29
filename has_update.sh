f769732d848fa9439376e2d9f2dc512e749e7b64
tomo_ing <tomoinate@gmail.com>
2025-05-29 20:14:07 +0900

    Update has_update.sh

--- has_update.sh
+++ has_update.sh
@@ -63,7 +63,7 @@ if [ "${new_ver}" != "${VERSION}" ]; then
   # conf.txtを更新
   OLD_VERSION=${new_ver}
   sed -e "s/ver='${new_ver}'/ver='${VERSION}'/" -e "s/old_ver='${old_ver}'/old_ver='${OLD_VERSION}'/" ${SCRIPT_DIR}/conf.txt > tmp
-  mv tmp ${SERVER_DIR}/conf.txt
+  mv tmp ${SCRIPT_DIR}/conf.txt
 
   #アップデート用シェルの呼び出し
   cd ${SCRIPT_DIR}
