7c753427d59b197ed2654a4df7b539e3fd1c5251
tomo_ing <tomoinate@gmail.com>
2025-05-29 16:22:01 +0900

    Update minecraft_start.sh

--- minecraft_start.sh
+++ minecraft_start.sh
@@ -1,51 +1,54 @@
 #!/bin/bash
 
+# スクリプト自身のディレクトリを取得
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+CONF_FILE=${SCRIPT_DIR}/conf.txt
+
+# conf.txt ファイルの存在確認
+if [ ! -f "$CONF_FILE" ]; then
+  echo "エラー: 設定ファイル '$CONF_FILE' が見つかりません。" >&2
+  exit 1
+fi
+
 # 設定ファイルから変数取得
-pass='Your Directory'
+# '^キー名=' で行を特定し、'='以降を取得、シングルクォートを除去後、数字とドット以外を削除
+new_ver=$(grep "^new_ver=" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//" | sed 's/[^0-9.]//g')
 
-config=${pass}/conf.txt
-old_ver=`grep 'old_ver' $config | sed -e 's/[^0-9.]//g'`
-new_ver=`grep 'new_ver' $config | sed -e 's/[^0-9.]//g'`
+# SERVER_DIR の取得 (例: SERVER_DIR='/home/minecraft')
+# '^キー名=' で行を特定し、'='以降を取得、シングルクォートを除去
+SERVER_DIR=$(grep "^SERVER_DIR=" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//")
 
-SESSION_NAME01=`grep 'SESSION_NAME01' $config | sed -e 's/^.\{16\}//' -e 's/.\{1\}$//'`
-# SESSION_NAME02=`grep 'SESSION_NAME02' $config | sed -e 's/^.\{16\}//' -e 's/.\{1\}$//'`
+# session_list の取得 (例: SESSION_NAME='s1, s2')
+# '^キー名=' で行を特定し、'='以降を取得、シングルクォートを除去 
+session_list=$(grep "^SESSION_NAME=" "$CONF_FILE" | cut -d'=' -f2- | sed "s/^'//;s/'$//")
 
-# 公式サイトからサーバーの最新のバージョン値取得
+if [ -z "$session_list" ]; then
+  echo "エラー: '$CONF_FILE' に SESSION_NAME の設定が見つからないか、値が空です。" >&2
+  exit 1
+fi
 
-VERSION=`curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" https://minecraft.net/en-us/download/server/bedrock/ 2>/dev/null | grep bin-linux/bedrock-server | sed -e 's/.*<a href=\"\(https:.*\/bin-linux\/.*\.zip\).*/\1/' -e 's/[^0-9.]//g' -e 's/^.\{2\}//' -e 's/.\{1\}$//'`
-# 現在のサーバーのバージョン値と最新のバージョン値を比較する
-# バージョン値が異なる場合アップデートを行う。そうでない場合はそのままサーバーを起動する
-if [ ${new_ver} != ${VERSION} ]; then
+# カンマを区切り文字としてセッション名を一つずつ処理
+#    - `IFS=','` で内部フィールドセパレータをカンマに設定。
+#    - `read -r -a sessions_array <<< "$raw_session_list"` でカンマ区切りの文字列を配列に格納。
+#      (`-r` はバックスラッシュを解釈しない、`-a array` で配列に読み込む)
+IFS=',' read -r -a sessions_array <<< "$session_list"
 
-  # conf.txtを更新
-  OLD_VERSION=${new_ver}
-  sed -e "s/ver='${new_ver}'/ver='${VERSION}'/" -e "s/old_ver='${old_ver}'/old_ver='${OLD_VERSION}'/" ${pass}/conf.txt > tmp
-  mv tmp ${pass}/conf.txt
-  cd ${pass}/
-
-  #アップデート用シェルの呼び出し
-  ./mcs_update.sh
-
-else
-
-  # bedrock_serverの起動
+# 各セッション名でサーバー起動
+for session_name in "${sessions_array[@]}"; do
   # bedrock_serverのディレクトリ
-  # verは設定ファイル内
-  SERVER_DIR01=${pass}/server/bedrock_server${new_ver}
-  # SERVER_DIR02=${pass}/server2/bedrock_server${new_ver}
+  SESSION_DIR=${pass}/${session_name}
+  SERVER_SESSION_DIR=${pass}/${session_name}/bedrock_server${new_ver}
 
   # SESSION_NAMEは設定ファイル内
-  cd ${SERVER_DIR01}
-  cp -pf ${pass}/server.properties server.properties
-  cp -pf ${pass}/permissions.json permissions.json
-  cp -pf ${pass}/allowlist.json allowlist.json
-  LD_LIBRARY_PATH=. screen -dmS ${SESSION_NAME01} ./bedrock_server > ./bedrock_server01.log &
+  cd ${SERVER_SESSION_DIR}
+  cp -pf ${SESSION_DIR}/server.properties server.properties
+  cp -pf ${SESSION_DIR}/permissions.json permissions.json
+  cp -pf ${SESSION_DIR}/allowlist.json allowlist.json
 
-  # sleep 30
+  # bedrock_serverの起動
+  LD_LIBRARY_PATH=. screen -dmS ${session_name} ./bedrock_server > ./${session_name}.log &
 
-  # cd ${SERVER_DIR02}
-  # cp -pf ${pass}/server.properties2 server.properties
-  # cp -pf ${pass}/permissions.json permissions.json
-  # LD_LIBRARY_PATH=. screen -dmS ${SESSION_NAME02} ./bedrock_server > ./bedrock_server02.log &
-
+  # サーバーの起動を待機
+  sleep 20
 fi
